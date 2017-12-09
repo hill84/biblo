de115f344d205e0b6ef838f698a9d62f5401aac7
Giuseppe Gerbino <giu.gerbino@gmail.com>
2017-12-09 18:17:43 +0100

    Firebase initial setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import "semantic-ui-css/semantic.min.css";
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from 'firebase';
+
+var config = {
+	apiKey: "AIzaSyDmzwyXa4bBotGhyXN3r5ZAchDmua8a5i0",
+	authDomain: "delibris-4fa3b.firebaseapp.com",
+	databaseURL: "https://delibris-4fa3b.firebaseio.com",
+	projectId: "delibris-4fa3b",
+	storageBucket: "delibris-4fa3b.appspot.com",
+	messagingSenderId: "144759497905"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
 	<Router>
