b0ea56ea5936cdf8be8416239c1d65ab7f63c68b
Giuseppe Gerbino <quilan@hotmail.it>
2018-08-08 19:08:04 +0200

    Remove query-string module

--- src/components/pages/bookContainer.js
+++ src/components/pages/bookContainer.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Book from '../book';
-import queryString from 'query-string';
 
 const BookContainer = props => {
-  const search = queryString.parse(props.location.search);
   return (
     <div id="BookContainerComponent">
-      <Book bid={props.match.params.bid} isEditing={search.edit} user={props.user} openSnackbar={props.openSnackbar} />
+      <Book bid={props.match.params.bid} user={props.user} openSnackbar={props.openSnackbar} />
     </div>
   );
 };
