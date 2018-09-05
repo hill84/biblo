42236e2c6f01e562daff5992076af8d46dcef37f
Giuseppe Gerbino <quilan@hotmail.it>
2018-09-05 22:59:22 +0200

    Minor link fix

--- src/components/footer.js
+++ src/components/footer.js
@@ -10,11 +10,11 @@ const Footer = () => (
           <div className="col hide-md"><p>© {appName} 2018</p></div>
           <div className="col-md-auto col-12 text-center-md text-right">
             <ul className="nolist inline-items info-row">
-              <li className="counter"><Link to="/chi-siamo">Chi siamo</Link></li>
-              <li className="counter"><Link to="/termini">Termini <span className="hide-md">e condizioni</span></Link></li>
+              <li className="counter"><Link to="/about">Chi siamo</Link></li>
+              <li className="counter"><Link to="/terms">Termini <span className="hide-md">e condizioni</span></Link></li>
               <li className="counter"><Link to="/privacy">Privacy <span className="hide-md">policy</span></Link></li>
               <li className="counter"><Link to="/cookie">Cookie <span className="hide-md">policy</span></Link></li>
-              <li className="counter"><Link to="/aiuto">Aiuto</Link></li>
+              <li className="counter"><Link to="/help">Aiuto</Link></li>
             </ul>
           </div>
         </div>
