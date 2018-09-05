31bf9c127059e511b91a08d213a5bd3622049536
Giuseppe Gerbino <quilan@hotmail.it>
2018-09-05 22:40:24 +0200

    Authors

--- src/components/authors.js
+++ src/components/authors.js
@@ -5,14 +5,18 @@ import Link from 'react-router-dom/Link';
 import { authorsRef } from '../config/firebase';
 import { numberType } from '../config/types';
 import { getInitials } from '../config/shared';
+import { icon } from '../config/icons';
 
 export default class Authors extends React.Component {
 	state = {
     items: null,
     count: 0,
-    desc: false,
+    desc: true,
     limit: this.props.limit || 10,
     loading: true,
+    page: 1,
+    pagination: false,
+    scrollable: true,
     size: this.props.size || 80
   }
 
@@ -26,7 +30,7 @@ export default class Authors extends React.Component {
 
   fetch = () => { 
     const { desc, limit } = this.state;
-    authorsRef.where('photoURL', '>', '').orderBy('photoURL', desc ? 'desc' : 'asc').limit(limit).get().then(snap => {
+    authorsRef.orderBy('photoURL', desc ? 'desc' : 'asc').limit(limit).get().then(snap => {
       if (!snap.empty) {
         const items = [];
         snap.forEach(item => items.push(item.data()));
@@ -46,7 +50,7 @@ export default class Authors extends React.Component {
   }
 	
 	render() {
-    const { items, loading, size } = this.state;
+    const { count, desc, items, limit, loading, page, pagination, scrollable, size } = this.state;
 
     if (!items || items.length < 1) {
       if (loading) { 
@@ -57,16 +61,51 @@ export default class Authors extends React.Component {
     }
 
 		return (
-      <div className="bubbles row shelf scrollable">
-        <div className="shelf-row hoverable-items">
-          {items.map(item => 
-            <Link to={`/author/${item.displayName}`} key={item.displayName} style={{minWidth: size}} className="bubble col">
-              <Avatar style={{width: size, height: size}} className="avatar centered" src={item.photoURL} alt={item.displayName}>{!item.photoURL && getInitials(item.displayName)}</Avatar>
-              <div className="name">{item.displayName}</div>
-            </Link>
-          )}
+      <React.Fragment>
+        <div className="head nav" role="navigation">
+          <span className="counter last title primary-text">Autori</span> {count !== 0 && <span className="count hide-xs">({count} scrittori)</span>} 
+          {!loading && count > 0 &&
+            <div className="pull-right">
+              {(pagination && count > limit) || scrollable ?
+                <Link to="/authors" className="btn sm flat counter">Vedi tutti</Link>
+              :
+                <button 
+                  className={`btn sm icon flat counter ${desc ? 'desc' : 'asc'}`} 
+                  title={desc ? 'Ascendente' : 'Discendente'} 
+                  onClick={this.onToggleDesc}>
+                  {icon.arrowDown()}
+                </button>
+              }
+              {pagination && count > limit &&
+                <React.Fragment>
+                  <button 
+                    disabled={page < 2 && 'disabled'} 
+                    className="btn sm clear prepend" 
+                    onClick={() => this.fetch('prev')} title="precedente">
+                    {icon.chevronLeft()}
+                  </button>
+                  <button 
+                    disabled={page > (count / limit) && 'disabled'} 
+                    className="btn sm clear append" 
+                    onClick={() => this.fetch('next')} title="successivo">
+                    {icon.chevronRight()}
+                  </button>
+                </React.Fragment>
+              }
+            </div>
+          }
         </div>
-      </div>
+        <div className="bubbles row shelf scrollable">
+          <div className="shelf-row hoverable-items">
+            {items.map(item => 
+              <Link to={`/author/${item.displayName}`} key={item.displayName} style={{minWidth: size}} className="bubble col">
+                <Avatar style={{width: size, height: size}} className="avatar centered" src={item.photoURL} alt={item.displayName}>{!item.photoURL && getInitials(item.displayName)}</Avatar>
+                <div className="name">{item.displayName}</div>
+              </Link>
+            )}
+          </div>
+        </div>
+      </React.Fragment>
 		);
 	}
 }
