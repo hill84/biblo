14d3cd226ddb2479517e38f5134731bbda2b0336
Giuseppe Gerbino <giu.gerbino@gmail.com>
2020-01-16 13:41:58 +0100

    🧵 New functions

--- functions/index.js
+++ functions/index.js
@@ -1,51 +1,60 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-/* const gcs = require('@google-cloud/storage')();
-const spawn = require('child-process-promise').spawn;
-const path = require('path');
-const os = require('os');
-const fs = require('fs'); */
+// const gcs = require('@google-cloud/storage')();
+// const spawn = require('child-process-promise').spawn;
+// const path = require('path');
+// const os = require('os');
+// const fs = require('fs');
 
 admin.initializeApp();
 
 // firebase deploy --only functions
 
-// const timestamp = snap.get('created_at');
-// const date = timestamp.toDate();
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
-
 // HELPERS
 const ff = functions.region('europe-west1').firestore;
 
-const count = (doc, change, collection, field, nestedField) => {
+const count = props => {
+  const { doc, change, collection = 'counters', field = 'count', nestedField } = props;
   let increment;
+
   if (change == 1 || change == -1) {
     increment = change;
   } else {
     if (change.after.exists && !change.before.exists) { increment = 1 } else 
     if (!change.after.exists && change.before.exists) { increment = -1 } else { return null };
   }
-  const countRef = admin.firestore().collection(collection || 'counters').doc(doc);
+
+  const countRef = admin.firestore().doc(`${collection}/${doc}`);
   const value = admin.firestore.FieldValue.increment(increment);
-  const data = { [field || 'count']: nestedField ? { [nestedField]: value } : value };
+  const data = { [field]: nestedField ? { [nestedField]: value } : value };
 
   return countRef.set(data, { merge: true });
-}
+};
 
 // REVIEWS
-exports.incrementReviews = ff.document('reviews/{bid}/reviewers/{uid}').onCreate(() => count('reviews', 1));
+exports.incrementReviews = ff.document('reviews/{bid}/reviewers/{uid}').onCreate(() => count({ doc: 'reviews', change: 1 }));
 
-exports.decrementReviews = ff.document('reviews/{bid}/reviewers/{uid}').onDelete(() => count('reviews', -1));
+exports.decrementReviews = ff.document('reviews/{bid}/reviewers/{uid}').onDelete(() => count({ doc: 'reviews', change: -1 }));
 
-exports.countUserReviews = ff.document('reviews/{bid}/reviewers/{uid}').onWrite((change, context) => count(context.params.uid, change, 'users', 'stats', 'reviews_num'));
+exports.countUserReviews = ff.document('reviews/{bid}/reviewers/{uid}').onWrite((change, context) => count({
+  doc: context.params.uid, change, collection: 'users', field: 'stats', nestedField: 'reviews_num' 
+}));
 
-exports.countBookReviews = ff.document('reviews/{bid}/reviewers/{uid}').onWrite((change, context) => count(context.params.bid, change, 'books', 'reviews_num'));
+exports.incrementBookReviews = ff.document('reviews/{bid}/reviewers/{uid}').onCreate((snap, context) => count({ 
+  doc: context.params.bid, change: 1, collection: 'books', field: 'reviews_num' 
+}));
+
+exports.decrementBookReviews = ff.document('reviews/{bid}/reviewers/{uid}').onDelete((snap, context) => count({
+  doc: context.params.bid, change: -1, collection: 'books', field: 'reviews_num' 
+}));
+
+exports.incrementReviewsComments = ff.document('reviews/{bid}/reviewers/{uid}/commenters/{cid}').onCreate((snap, context) => count({ 
+  doc: context.params.uid, change: 1, collection: `reviews/${context.params.bid}/reviewers`, field: 'comments_num' 
+}));
+
+exports.decrementReviewsComments = ff.document('reviews/{bid}/reviewers/{uid}/commenters/{cid}').onDelete((snap, context) => count({
+  doc: context.params.uid, change: -1, collection: `reviews/${context.params.bid}/reviewers`, field: 'comments_num' 
+}));
 
 /* exports.feedReviews = ff.document('reviews/{bid}/reviewers/{uid}').onWrite((change, context) => {
   const { bid } = context.params;
@@ -77,15 +86,17 @@ exports.truncateFeedReviews = ff.document('reviews/{bid}/reviewers/{uid}').onCre
 }); */
 
 // NOTIFICATIONS
-exports.incrementNotifications = ff.document('notifications/{nid}').onCreate(() => count('notifications', 1));
+exports.incrementNotifications = ff.document('notifications/{nid}').onCreate(() => count({ doc: 'notifications', change: 1 }));
 
-exports.decrementNotifications = ff.document('notifications/{nid}').onDelete(() => count('notifications', -1));
+exports.decrementNotifications = ff.document('notifications/{nid}').onDelete(() => count({ doc: 'notifications', change: -1 }));
 
-/* exports.countNotes = ff.document('notifications/{uid}/notes/{nid}').onWrite((change, context) => count(context.params.uid, change, 'notifications')); */
+exports.incrementNotes = ff.document('notifications/{uid}/notes/{nid}').onCreate((snap, context) => count({ 
+  doc: context.params.uid, change: 1, collection: 'notifications' 
+}));
 
-exports.incrementNotes = ff.document('notifications/{uid}/notes/{nid}').onCreate((snap, context) => count(context.params.uid, 1, 'notifications'));
-
-exports.decrementNotes = ff.document('notifications/{uid}/notes/{nid}').onDelete((snap, context) => count(context.params.uid, -1, 'notifications'));
+exports.decrementNotes = ff.document('notifications/{uid}/notes/{nid}').onDelete((snap, context) => count({
+  doc: context.params.uid, change: -1, collection: 'notifications' 
+}));
 
 /* exports.clearSubNotes = ff.document('notifications/{uid}').onDelete((snap, context) => {
   const { uid } = context.params;
@@ -95,16 +106,22 @@ exports.decrementNotes = ff.document('notifications/{uid}/notes/{nid}').onDelete
 }); */
 
 // COLLECTIONS
-exports.incrementCollections = ff.document('collections/{cid}').onCreate(() => count('collections', 1));
+exports.incrementCollections = ff.document('collections/{cid}').onCreate(() => count({ doc: 'collections', change: 1 }));
 
-exports.decrementCollections = ff.document('collections/{cid}').onDelete(() => count('collections', -1));
+exports.decrementCollections = ff.document('collections/{cid}').onDelete(() => count({ doc: 'collections', change: -1 }));
 
-exports.countCollectionBooks = ff.document('collections/{cid}/books/{bid}').onWrite((change, context) => count(context.params.cid, change, 'collections', 'books_num'));
+exports.incrementCollectionBooks = ff.document('collections/{cid}/books/{bid}').onCreate(() => count({ 
+  doc: context.params.cid, change: 1, collection: 'collections', field: 'books_num' 
+}));
+
+exports.decrementCollectionBooks = ff.document('collections/{cid}/books/{bid}').onDelete(() => count({ 
+  doc: context.params.cid, change: -1, collection: 'collections', field: 'books_num' 
+}));
 
 // BOOKS
-exports.incrementBooks = ff.document('books/{bid}').onCreate(() => count('books', 1));
+exports.incrementBooks = ff.document('books/{bid}').onCreate(() => count({ doc: 'books', change: 1 }));
 
-exports.decrementBooks = ff.document('books/{bid}').onDelete(() => count('books', -1));
+exports.decrementBooks = ff.document('books/{bid}').onDelete(() => count({ doc: 'books', change: -1 }));
 
 exports.clearBook = ff.document('books/{bid}').onDelete((snap, context) => {
   const { bid } = context.params;
@@ -116,38 +133,51 @@ exports.clearBook = ff.document('books/{bid}').onDelete((snap, context) => {
     });
   }
 
-  const ReviewsRef = admin.firestore().collection('reviews').doc(bid);
+  const ReviewRef = admin.firestore().collection('reviews').doc(bid);
 
-  ReviewsRef.collection('reviewers').get().then(snap => {
+  const ReviewersRef = ReviewRef.collection('reviewers');
+
+  ReviewersRef.get().then(snap => {
     if (!snap.empty) {
       snap.forEach(reviewer => {
-        ReviewsRef.collection('reviewers').doc(reviewer.id).delete(); // delete book reviews
+        const ReviewerRef = ReviewersRef.doc(reviewer.id);
+
+        const reviewerCommentersRef = ReviewerRef.collection('commenters');
+
+        reviewerCommentersRef.get().then(snap => {
+          if (!snap.empty) {
+            snap.forEach(commenter => {
+              reviewerCommentersRef.doc(commenter.id).delete(); // delete comments
+            });
+          } else { ReviewerRef.delete(); } // delete reviewers
+        });
+
       });
-    } else { ReviewsRef.delete(); } // delete book from reviews
+    } else { ReviewRef.delete(); } // delete reviews
   });
   return admin.storage().bucket().deleteFiles({ prefix: `books/${bid}` }); // delete folder in storage
 });
 
 // USERS
-exports.incrementUsers = ff.document('users/{uid}').onCreate(() => count('users', 1));
+exports.incrementUsers = ff.document('users/{uid}').onCreate(() => count({ doc: 'users', change: 1 }));
 
-exports.decrementUsers = ff.document('users/{uid}').onDelete(() => count('users', -1));
+exports.decrementUsers = ff.document('users/{uid}').onDelete(() => count({ doc: 'users', change: -1 }));
 
 exports.clearUserAuth = ff.document('users/{uid}').onDelete((snap, context) => admin.auth().deleteUser(context.params.uid));
 
 exports.clearUserFiles = ff.document('users/{uid}').onDelete((snap, context) => admin.storage().bucket().deleteFiles({ prefix: `users/${context.params.uid}` }));
 
 // AUTHORS
-exports.incrementAuthors = ff.document('authors/{aid}').onCreate(() => count('authors', 1));
+exports.incrementAuthors = ff.document('authors/{aid}').onCreate(() => count({ doc: 'authors', change: 1 }));
 
-exports.decrementAuthors = ff.document('authors/{aid}').onDelete(() => count('authors', -1));
+exports.decrementAuthors = ff.document('authors/{aid}').onDelete(() => count({ doc: 'authors', change: -1 }));
 
 // QUOTES
-exports.incrementQuotes = ff.document('quotes/{qid}').onCreate(() => count('quotes', 1));
+exports.incrementQuotes = ff.document('quotes/{qid}').onCreate(() => count({ doc: 'quotes', change: 1 }));
 
-exports.decrementQuotes = ff.document('quotes/{qid}').onDelete(() => count('quotes', -1));
+exports.decrementQuotes = ff.document('quotes/{qid}').onDelete(() => count({ doc: 'quotes', change: -1 }));
 
 // CHALLENGES
-exports.incrementChallenges = ff.document('challenges/{cid}').onCreate(() => count('challenges', 1));
+exports.incrementChallenges = ff.document('challenges/{cid}').onCreate(() => count({ doc: 'challenges', change: 1 }));
 
-exports.decrementChallenges = ff.document('challenges/{cid}').onDelete(() => count('challenges', -1));
+exports.decrementChallenges = ff.document('challenges/{cid}').onDelete(() => count({ doc: 'challenges', change: -1 }));
